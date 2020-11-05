5e6d7d62132937dc1b23f09fd4d6621a03981668
simarroguerapedro@gmail.com <simarroguerapedro@gmail.com>
2020-11-05 13:31:29 +0100

    cambios mínimos

--- tarea-1/controlador.py
+++ tarea-1/controlador.py
@@ -10,18 +10,18 @@ def main():
 	# creamos subproceso
 	proc = subprocess.Popen(["./controlado"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True, bufsize=1)
 	
-	# thread que maneja la entrada la entrada del subproceso 
+	# thread que maneja la entrada del subproceso 
 	t_input = threading.Thread(target=input_handler, args=[proc.stdin], daemon=True)
 	t_input.start()
 	
 	# thread que maneja la salida del subproceso
-	t_output = threading.Thread(target=output_handler, args=[proc], daemon=True)
+	t_output = threading.Thread(target=output_handler, args=[proc.stdout], daemon=True)
 	t_output.start()
 	
 	try: 
 		#mantenemos vivo thread principal
 		while(True):
-			time.sleep(1)
+			continue
 	except KeyboardInterrupt:
 		print ("\nKeyboard Interrupt")
 		sys.exit(0)
@@ -36,13 +36,15 @@ def input_handler(stdin):
 		time.sleep(1)
 
 # manejador de la salida: comprueba constantemente la salida del subproceso y la muestra si existe.
-def output_handler(proc):
+def output_handler(stdout):
 	while True:
-		output = proc.stdout.readline()
-		if proc.poll() is not None: # no necesario ya que subproceso es un bucle infinito y nunca termina
-			break
+		output = stdout.readline() #bloqueante
+		#if proc.poll() is not None: # no necesario ya que subproceso es un bucle infinito y nunca termina
+			#break
 		if output:
 			print (output.strip())
+		else:
+			break
 		
 		   
 if __name__ == "__main__": 
