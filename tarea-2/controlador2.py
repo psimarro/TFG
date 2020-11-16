7ba1b84d79cd2db78d3e62e3c30cd7c2ca903e89
Pedro Simarro <simarroguerrapedro@gmail.com>
2020-11-16 05:16:45 +0100

    cambios controlador2.py

--- tarea-2/controlador2.py
+++ tarea-2/controlador2.py
@@ -1,14 +1,18 @@
 import subprocess
 import threading
+import multiprocessing
 import random
 import time
 import sys
 
+nCores = multiprocessing.cpu_count()
+kvazaar_path = "/home/pedro/malleable_kvazaar/bin/./kvazaar"
+vid_path = "/home/pedro/Descargas/E_KristenAndSara_1280x720_60p.yuv"
+
 def main():
 	#subprocess.run(["g++", "controlado.cpp", "-o", "controlado"]) # compilamos controlado.cpp
 	
-	comando = ["/home/pedro/malleable_kvazaar/bin/./kvazaar", 
-	"--input", "/home/pedro/Descargas/E_KristenAndSara_1280x720_60p.yuv", "--output", "/dev/null", "--preset=ultrafast", "--qp=22", "--owf=0", "--threads=6"]
+	comando = [kvazaar_path, "--input", vid_path, "--output", "/dev/null", "--preset=ultrafast", "--qp=22", "--owf=0", "--threads=" + str(nCores)]
 	# creamos subproceso
 	proc = subprocess.Popen(comando, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True, bufsize=1, env={'NUM_FRAMES_PER_BATCH': '24'})
 	
@@ -33,7 +37,7 @@ def main():
 def input_handler(stdin, stdout):
 	random.seed() #generamos la semilla para randoms
 	while(True):
-		s = "nThs:" + str(random.randint(1,6)) 
+		s = "nThs:" + str(random.randint(1, nCores)) 
 		print(s)
 		stdin.write(s + "\n")
 		output = stdout.readline()
