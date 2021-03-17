e316f982ac6c2d2e3bdc2e637a169843e1713010
Pedro Simarro <simarroguerrapedro@gmail.com>
2021-03-17 16:02:18 +0100

    Añadidos parametros al train y learned. Modos de ejecución aleatorio y rotatorio

--- learned_kvazaar.py
+++ learned_kvazaar.py
@@ -4,31 +4,36 @@ import gym
 import os
 import ray
 import ray.rllib.agents.ppo as ppo
-import shutil
 import multiprocessing
 import glob
 import getpass
 import subprocess
+import argparse
 
 
 nCores = multiprocessing.cpu_count()
 
 def main ():
+
+    args, parser = parse_args()
+
     ##kvazaar options
-    user = getpass.getuser()
-    kvazaar_path = "/home/" + user + "/malleable_kvazaar/bin/./kvazaar"
-    vids_path_test = "/home/" + user + "/videos_train_test/"
-    kvazaar_cores = [x for x in range(int(nCores/2),int(nCores))]
-    print(kvazaar_cores)
+    kvazaar_path = args.kvazaar
+    vids_path_test = args.videos
+    kvazaar_cores = calcula_cores(args.cores[0],args.cores[1])
+    
 
+    ##Set affinity of main process using cores left by kvazaar
+    set_affinity(kvazaar_cores)
+    
     # start Ray -- add `local_mode=True` here for debugging
-    ray.init(ignore_reinit_error=True)
+    ray.init(ignore_reinit_error=True, local_mode=True)
 
-# configure the environment and create agent
+    #configure the environment and create agent
     config = ppo.DEFAULT_CONFIG.copy()
-    config["log_level"] = "WARN"
-    config["num_workers"] = 1
-    config["num_cpus_per_worker"] = nCores/2
+    config["log_level"] = "WARN"    
+    config["num_workers"] = 0
+    config["num_cpus_for_driver"] = nCores - len(kvazaar_cores)
     config["train_batch_size"] = 200
     config["rollout_fragment_length"] =  200
 
@@ -37,11 +42,22 @@ def main ():
     select_env = "kvazaar-v0"
     register_env(select_env, lambda config: Kvazaar_v0(kvazaar_path=kvazaar_path, 
                                                        vids_path=vids_path_test, 
-                                                       cores=kvazaar_cores))
+                                                       cores=kvazaar_cores,
+                                                       mode=None))
 
     agent = ppo.PPOTrainer(config, env=select_env)
 
-    chkpt_file = max(glob.iglob("./tmp/exa/*/*[!.tune_metadata]") , key=os.path.getctime)
+
+    #restore checkpoint
+    chkpnt_root = args.path
+    print(chkpnt_root)
+    if(chkpnt_root[len(chkpnt_root)-1] != '/'): chkpnt_root += "/"
+    if(chkpnt_root == parser.get_default("-p") or chkpnt_root == "tmp/exa/"): chkpnt_root += "*"
+    print(parser.get_default("-p"))
+    print(parser)
+    print(chkpnt_root)
+    chkpt_file = max(glob.iglob(chkpnt_root + "/*/*[!.tune_metadata]") , key=os.path.getctime) ##retrieve last checkpoint path
+    
     print(('----------------------\n' +
             ' ---------------------\n' +
             'checkpoint loaded --   {:} \n'+
@@ -79,8 +95,28 @@ def main ():
     
 
 
-if __name__ == "__main__":
+def parse_args():
+    user = getpass.getuser()
+    parser = argparse.ArgumentParser(description="Ray agent launcher that restores a checkpoint training of Kvazaar gym model.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-k", "--kvazaar", help="Kvazaar's executable file location.", default="/home/" + user + "/malleable_kvazaar/bin/./kvazaar")
+    parser.add_argument("-v", "--videos", help= "Path of the tested video. If it is a directory, a random one will be choosed.", default= "/home/" + user + "/videos_kvazaar_test/")
+    parser.add_argument("-c", "--cores", nargs=2, metavar=('core_ini', 'core_fin'), type=int, help= "Kvazaar's dedicated CPUS (range)", default=[int(nCores/2), nCores-1])
+    parser.add_argument("-p", "--path", type=str, help="Checkpoints path", default="tmp/exa/")
+    args = parser.parse_args()
+    return args, parser
+
+def calcula_cores(core_ini, core_fin):
+    print(core_ini, core_fin)
+    return [x for x in range(core_ini,core_fin+1)] 
+
+def set_affinity(kvazaar_cores):
     pid = os.getpid()
-    p = subprocess.Popen(["taskset", "-p", str(pid), "-c", "0-" + str(int(nCores/2) - 1)])
+    print("Curren pid: ", pid)
+    total_cores = [x for x in range (nCores)]
+    cores_main_proc = [x for x in total_cores if x not in kvazaar_cores]
+    p = subprocess.Popen(["taskset", "-cp", ",".join(map(str,cores_main_proc)), str(pid)])
     p.wait()
+
+if __name__ == "__main__":
+    
     main()
--- train_kvazaar.py
+++ train_kvazaar.py
@@ -12,25 +12,32 @@ import multiprocessing
 import getpass
 import sys
 import subprocess
+import argparse
+import datetime
 
 
 nCores = multiprocessing.cpu_count()
 
 def main ():
+
+    args = parse_args()
+
     ##kvazaar options
-    user = getpass.getuser()
-    kvazaar_path = "/home/" + user + "/malleable_kvazaar/bin/./kvazaar"
-    vids_path_train = "/home/" + user + "/videos_kvazaar/"
-    vids_path_test = "/home/" + user + "/videos_train_test/"
-    kvazaar_cores = [x for x in range(int(nCores/2),int(nCores))]
+    kvazaar_path = args.kvazaar
+    vids_path_train = args.videos
+    kvazaar_cores = calcula_cores(args.cores[0],args.cores[1])
+    kvazaar_mode = args.mode
+
+    ##Set affinity of main process using cores left by kvazaar
+    set_affinity(kvazaar_cores)
 
     # init directory in which to save checkpoints
-    chkpt_root = "tmp/exa"
-    shutil.rmtree(chkpt_root, ignore_errors=True, onerror=None)
+    chkpt_root = "tmp/exa/" + args.name + "/"
+    # shutil.rmtree(chkpt_root, ignore_errors=True, onerror=None)
 
     # init directory in which to log results
-    ray_results = "{}/ray_results/".format(os.getenv("HOME"))
-    shutil.rmtree(ray_results, ignore_errors=True, onerror=None)
+    # ray_results = "{}/ray_results/{}/".format(os.getenv("HOME"), args.name)
+    # shutil.rmtree(ray_results, ignore_errors=True, onerror=None)
 
 
     # start Ray -- add `local_mode=True` here for debugging
@@ -41,14 +48,15 @@ def main ():
     select_env = "kvazaar-v0"
     register_env(select_env, lambda config: Kvazaar_v0(kvazaar_path=kvazaar_path, 
                                                        vids_path=vids_path_train, 
-                                                       cores=kvazaar_cores))
+                                                       cores=kvazaar_cores,
+                                                       mode=kvazaar_mode))
 
 
     # configure the environment and create agent
     config = ppo.DEFAULT_CONFIG.copy()
     config["log_level"] = "WARN"    
     config["num_workers"] = 0
-    config["num_cpus_for_driver"] = int(nCores/2)
+    config["num_cpus_for_driver"] = nCores - len(kvazaar_cores)
     config["train_batch_size"] = 200
     config["rollout_fragment_length"] =  200
          
@@ -56,7 +64,7 @@ def main ():
     agent = ppo.PPOTrainer(config, env=select_env)
 
     status = "\033[1;32;40m{:2d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:4.2f} saved {}\033[0m"
-    n_iter = int(sys.argv[1])
+    n_iter = args.iters
 
     # train a policy with RLlib using PPO
     for n in range(n_iter):
@@ -74,48 +82,38 @@ def main ():
                     ))
         except KeyboardInterrupt:
             agent.stop()
+        except StopIteration:
+            print("Training stopped.") 
+            exit()
 
+ 
 
-    # examine the trained policy
-    policy = agent.get_policy()
-    model = policy.model
-    print(model.base_model.summary())
+def parse_args():
+    user = getpass.getuser()
+    parser = argparse.ArgumentParser(description="Trainer for Kvazaar video encoder using RLLIB.",
+    argument_default=argparse.SUPPRESS, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--iters", type=int, help="Number of training iterations", required=True)
+    parser.add_argument("-b" "--batch", type=int, help="Size of training batch", default=200)
+    parser.add_argument("-m", "--mode", help="Mode of video selection", choices=["random","rotating"], required=True)
+    parser.add_argument("-k", "--kvazaar", help="Kvazaar's executable file location", default="/home/" + user + "/malleable_kvazaar/bin/./kvazaar")
+    parser.add_argument("-v", "--videos", help= "Path of the set of videos for training", default= "/home/" + user + "/videos_kvazaar_train/")
+    parser.add_argument("-c", "--cores", nargs=2, metavar=('start', 'end'), type=int, help= "Kvazaar's dedicated CPUS (range)", default=[int(nCores/2), nCores-1])
+    parser.add_argument("-n", "--name", help="Name of the trainiing. This will be the name of the path for saving checkpoints.", default=datetime.datetime.now().strftime("%d_%m_%Y__%H_%M_%S"))
+    args = parser.parse_args()
+    return args
 
-    #agent.cleanup() ##clean worker so kvazaar instances are shut down
-    # apply the trained policy in a rollout
-    agent.restore(chkpt_file)
-    env = gym.make(select_env, 
-                   kvazaar_path=kvazaar_path, 
-                   vids_path=vids_path_test, 
-                   cores=kvazaar_cores)
-    
-    state = env.reset()
-    sum_reward = 0
-    done = None
-    cpus_used = [0] * len(kvazaar_cores)
-    
-    while not done:
-        action = agent.compute_action(state)
-        print("action:", action+1, "core(s)")
-        cpus_used[action] += 1
-        state, reward, done, info = env.step(action)
-        sum_reward += reward
+def calcula_cores(core_ini, core_fin):
+    print(core_ini, core_fin)
+    return [x for x in range(core_ini,core_fin+1)] 
 
-        if not done:
-            env.render()
-        else:
-            # report at the end of each episode
-            print("cumulative reward", sum_reward, "cpus used:", cpus_used)
-    
-    env.close() # close kvazaar
-   
-
+def set_affinity(kvazaar_cores):
+    pid = os.getpid()
+    print("Current pid: ", pid)
+    total_cores = [x for x in range (nCores)]
+    cores_main_proc = [x for x in total_cores if x not in kvazaar_cores]
+    p = subprocess.Popen(["taskset", "-cp", ",".join(map(str,cores_main_proc)), str(pid)])
+    p.wait()
 
 if __name__ == "__main__":
-    id = os.getpid()
-    print(id)
-    cores_main = [x for x in range (int(nCores/2))]
-    p = subprocess.Popen(["taskset", "-cp", ",".join(map(str,cores_main)), str(id)])
-    p.wait()
     main()
 
